f6c89f660e50b5b028fe3629528330b3b08668f9
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-06-23 15:55:27 -0500

    Switch to minitest/spec style
    Because:
    
    * I prefer it
    
    Solution:
    
    * Replace the testunit code with spec
    * Remove the purposely failing test

--- test/rack/relations_test.rb
+++ test/rack/relations_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
-class Rack::RelationsTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Rack::Relations::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+describe Rack::Relations do
+  describe "::VERSION" do
+    it "has a version number" do
+      refute_nil ::Rack::Relations::VERSION
+    end
   end
 end
